91dfe93d068723c24ae3b0e4ae4a2e723076531f
irenicaa <root@irenica.pro>
2020-11-30 18:41:28 +0300

    Add a test handler

--- cmd/go-dice-generator/main.go
+++ cmd/go-dice-generator/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		log.Print("received a request at " + request.URL.String())
+		writer.Write([]byte("Hello, user!"))
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
