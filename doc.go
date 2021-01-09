a794e370679255e0d78a8814c08c0753eaa7e59e
irenicaa <root@irenica.pro>
2021-01-10 00:36:48 +0300

    Add generating of API docs

---
+++ doc.go
@@ -0,0 +1,8 @@
+package dicegenerator
+
+//go:generate swag init --generalInfo doc.go --propertyStrategy pascalcase
+
+// @title go-dice-generator API
+// @version 1.0.0
+// @license.name MIT
+// @host localhost:8080
